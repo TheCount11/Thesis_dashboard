eb32edf8f31217e0e180bf0354a8c6e5ae197b6e
TheCount11 <sagnik.rick7@gmail.com>
2021-07-23 18:55:03 +0200

    added image files

--- main.py
+++ main.py
@@ -344,8 +344,9 @@ with st.beta_expander('Spatial, Temporal, Topical'):
 with st.beta_expander('P.S.'):
 	st.write("The spatial facet does not allow for selection by location, but uses language for the following image")
 	file_path = Path(__file__).parents[0]/ 'data' / 'lang-loc-uk.jpeg'
-	img = Image.open(file_path) 
-	st.image(img)
+	#img = Image.open(file_path) 
+	st.write(file_path)
+	st.image(file_path)
 
 
 
