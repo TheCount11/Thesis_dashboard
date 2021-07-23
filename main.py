34838ad23f4d3b7aa115143957343a654c929fa4
TheCount11 <sagnik.rick7@gmail.com>
2021-07-23 18:37:17 +0200

    changed path to string

--- main.py
+++ main.py
@@ -344,7 +344,7 @@ with st.beta_expander('Spatial, Temporal, Topical'):
 with st.beta_expander('P.S.'):
 	st.write("The spatial facet does not allow for selection by location, but uses language for the following image")
 	file_path = Path(__file__).parents[0]/ 'data'
-	img = file_path \ 'lang-loc-uk.jpeg'
+	img = file_path / 'lang-loc-uk.jpeg'
 	st.image(img)
 
 
